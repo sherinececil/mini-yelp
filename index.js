7b9947ec6dd77f39af2e40f64e2bbb9745a1e9dc
Natalia Trzeciak <trzeciak.natalia@icloud.com>
2021-09-11 10:04:45 +0200

    cors fixed

--- index.js
+++ index.js
@@ -13,15 +13,8 @@ const PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: "https://mini-yelp-group2.herokuapp.com/" 
-}))
-app.use(cors({
-  origin: "http://localhost:5000/" 
-}))
-app.use(cors({
-  origin: "http://localhost:3000/" 
-}))
+
+app.use(cors())
 
 app.use("/restaurants", restaurantsRouter);
 app.use("/cities", citiesRouter);
